ada357b7c617297e9891563c62fa7b18b26c7f7e
trhyfgh <trhyfgh@163.com>
2015-06-03 19:17:02 +0900

    Update with scheduler
    try register as windows service and schedule job using Quartz .net.

---
+++ OncallPushService/DoPush.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using Quartz.Impl;
+using Common.Logging;
+
+namespace OncalTimePush
+{
+    public class DoPush:IJob
+    {
+        //private static ILog onCallLog = LogManager.GetLogger(typeof(OntimePush));
+        
+        private static readonly ILog pushLog = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Execute(IJobExecutionContext context)
+        {
+            //add push logic in this part
+        }
+    }
+   
+}
---
+++ OncallPushService/ScheduleManage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Quartz.Impl;
+using Quartz;
+
+namespace OncallPushService
+{
+    public partial class ScheduleManage : ServiceBase
+    {
+        private readonly ILog scheduleLoger;
+        private IScheduler scheduler;
+
+        public ScheduleManage()
+        {
+            InitializeComponent();
+            scheduleLoger = LogManager.GetLogger(GetType());
+            ISchedulerFactory pushSchedulerfactory = new StdSchedulerFactory();
+            scheduler = pushSchedulerfactory.GetScheduler();
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            scheduler.Start();
+            scheduleLoger.Info("-------------------scheduler start----------------------");
+        }
+
+        protected override void OnStop()
+        {
+            scheduler.Shutdown();
+            scheduleLoger.Info("-------------------scheduler stop-----------------------");
+            
+        }
+    }
+}
