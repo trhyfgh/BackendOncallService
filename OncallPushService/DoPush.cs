67873b412da88f00a918b0a4b433bffe3e9346fe
trhyfgh <trhyfgh@163.com>
2015-06-04 19:05:48 +0900

    Normal updates
    Nothing special.

--- OncallPushService/DoPush.cs
+++ OncallPushService/DoPush.cs
@@ -5,8 +5,24 @@ using System.Web;
 using Quartz;
 using Quartz.Impl;
 using Common.Logging;
+using BackendOncallService.Controllers;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json;
 
-namespace OncalTimePush
+
+using cn.jpush.api;
+using cn.jpush.api.push;
+using cn.jpush.api.report;
+using cn.jpush.api.common;
+using cn.jpush.api.util;
+using cn.jpush.api.push.mode;
+using cn.jpush.api.push.notification;
+using cn.jpush.api.common.resp;
+
+namespace OncallPushService
 {
     public class DoPush:IJob
     {
@@ -14,10 +30,147 @@ namespace OncalTimePush
         
         private static readonly ILog pushLog = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        //Jpush Message define.
+        public static String TITLE = "Oncall Reminder";
+        public static String ALERT = "Oncall Reminde - alert";
+        public static String MSG_CONTENT = "You are going to take the oncall";
+        public static String REGISTRATION_ID = "0900e8d85ef";
+        public static String TAG = "tag_api";
+        public static String app_key = "997f28c1cea5a9f17d82079a";
+        public static String master_secret = "47d264a3c02a6a5a4a256a45";
+        
         public void Execute(IJobExecutionContext context)
         {
             //add push logic in this part
+            string pushToWhome = getOncallSupport();
+            Console.WriteLine("*****开始发送******");
+            JPushClient client = new JPushClient(app_key, master_secret);
+            PushPayload payload = PushObject_All_All_Alert();
+            try
+            {
+                var result = client.SendPush(payload);
+                //由于统计数据并非非是即时的,所以等待一小段时间再执行下面的获取结果方法
+                System.Threading.Thread.Sleep(10000);
+                /*如需查询上次推送结果执行下面的代码*/
+                var apiResult = client.getReceivedApi(result.msg_id.ToString());
+                var apiResultv3 = client.getReceivedApi_v3(result.msg_id.ToString());
+                /*如需查询某个messageid的推送结果执行下面的代码*/
+                var queryResultWithV2 = client.getReceivedApi("1739302794");
+                var querResultWithV3 = client.getReceivedApi_v3("1739302794");
+
+            }
+            catch (APIRequestException e)
+            {
+                Console.WriteLine("Error response from JPush server. Should review and fix it. ");
+                Console.WriteLine("HTTP Status: " + e.Status);
+                Console.WriteLine("Error Code: " + e.ErrorCode);
+                Console.WriteLine("Error Message: " + e.ErrorCode);
+            }
+            catch (APIConnectionException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("*****结束发送******");
         }
+
+        public string getOncallSupport()
+        {
+            DateTime now = new DateTime();
+            string seq = now.ToString().Substring(9, 2);
+            OncallCellsController oc = new OncallCellsController();
+            var url = "/api/OncallCells/5";
+            
+            //HttpWebRequest 
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); 
+           
+            //httpClient
+            HttpClient httpClient = new HttpClient();
+            //HttpContent httpContent;
+            //httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            httpClient.BaseAddress = new Uri(url);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            //httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeHeaderValue("application/json"));\
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var rsp =  httpClient.GetAsync(url);
+            //HttpResponseMessag> responses =  httpClient.GetAsync(url);
+            
+            return null;
+        }
+            
+        public static PushPayload PushObject_All_All_Alert()
+        {
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.all();
+            pushPayload.audience = Audience.all();
+            pushPayload.notification = new Notification().setAlert(ALERT);
+            return pushPayload;
+        }
+        public static PushPayload PushObject_all_alias_alert()
+        {
+
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android();
+            pushPayload.audience = Audience.s_alias("alias1");
+            pushPayload.notification = new Notification().setAlert(ALERT);
+            return pushPayload;
+
+        }
+        public static PushPayload PushObject_Android_Tag_AlertWithTitle()
+        {
+            PushPayload pushPayload = new PushPayload();
+
+            pushPayload.platform = Platform.android();
+            pushPayload.audience = Audience.s_tag("tag1");
+            pushPayload.notification = Notification.android(ALERT, TITLE);
+
+            return pushPayload;
+        }
+        public static PushPayload PushObject_android_and_ios()
+        {
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android_ios();
+            var audience = Audience.s_tag("tag1");
+            pushPayload.audience = audience;
+            var notification = new Notification().setAlert("alert content");
+            notification.AndroidNotification = new AndroidNotification().setTitle("Android Title");
+            notification.IosNotification = new IosNotification();
+            notification.IosNotification.incrBadge(1);
+            notification.IosNotification.AddExtra("extra_key", "extra_value");
+
+            pushPayload.notification = notification.Check();
+
+
+            return pushPayload;
+        }
+        public static PushPayload PushObject_ios_tagAnd_alertWithExtrasAndMessage()
+        {
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android_ios();
+            pushPayload.audience = Audience.s_tag_and("tag1", "tag_all");
+            var notification = new Notification();
+            notification.IosNotification = new IosNotification().setAlert(ALERT).setBadge(5).setSound("happy").AddExtra("from", "JPush");
+
+            pushPayload.notification = notification;
+            pushPayload.message = Message.content(MSG_CONTENT);
+            return pushPayload;
+
+        }
+        public static PushPayload PushObject_ios_audienceMore_messageWithExtras()
+        {
+
+            var pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android_ios();
+            pushPayload.audience = Audience.s_tag("tag1", "tag2");
+            pushPayload.message = Message.content(MSG_CONTENT).AddExtras("from", "JPush");
+            return pushPayload;
+
+        }
+
     }
    
 }
